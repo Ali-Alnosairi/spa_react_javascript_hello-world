fc48552b8d81303d4bc894a1a91eaa1b013133fe
Ali-Alnosairi <alialnosiari@gmail.com>
2024-09-08 07:07:30 +0300

    Add logout button component

--- src/components/navigation/desktop/nav-bar.js
+++ src/components/navigation/desktop/nav-bar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { NavBarBrand } from "./nav-bar-brand";
-import { NavBarButtons } from "./nav-bar-buttons";
 import { NavBarTabs } from "./nav-bar-tabs";
 import LoginButton from "../../LoginButton";
+import LogoutButton from "../../LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 export const NavBar = () => {
+  const { isAuthenticated } = useAuth0(); 
   return (
     <div className="nav-bar__container">
       <nav className="nav-bar">
         <NavBarBrand />
         <NavBarTabs />
-        <NavBarButtons />
-        <LoginButton />
+        {!isAuthenticated && (
+          <>
+            <LoginButton/>
+          </>
+        )}
+        {isAuthenticated && (
+          <>
+            <LogoutButton/>
+          </>
+        )}
       </nav>
     </div>
   );
